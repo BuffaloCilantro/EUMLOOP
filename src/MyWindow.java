d6c511642b5e7870e285ee33902ac273b087f009
Eugene Kim <eugene0127@gmail.com>
2019-06-05 09:58:25 -0700

    worked in class for a bit

--- src/MyWindow.java
+++ src/MyWindow.java
@@ -1,20 +1,25 @@
-import java.util.ArrayList;
+import java.awt.Color;
+import java.util.HashMap;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 
 public class MyWindow extends JFrame{
 		JMenuBar mBar = new JMenuBar();
-		JMenu editClasses = new JMenu();
-		ArrayList<EClass> MyEClasses = new ArrayList<EClass>();
-	
-	public MyWindow(String title) {
+		JMenu editClasses = new JMenu("Edit");
+		HashMap<String, EClass> hMap = new HashMap<>();
+		JButton button = new JButton("Edit");
 		
+	public MyWindow(String title) {
 		super(title);
 		setSize(800, 600);
 		setVisible(true);
 		setLocationRelativeTo(null);
-		
-		editClasses.setText("Edit");
+		mBar.setBackground(Color.RED);
+		mBar.add(button);
+		setJMenuBar(mBar);
+		setVisible(true);
 	}
 }
--- src/main.java
+++ src/main.java
@@ -2,7 +2,7 @@ public class main {
 
 	public static void main(String[] args) {
 		MyWindow w = new MyWindow("EUMPLOOP");
+		w.setVisible(true);
 		System.out.println("Success!");
 	}
-
 }
