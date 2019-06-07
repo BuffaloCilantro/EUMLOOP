4792cc1c65a67a23fb1f14f2d61fe2b23e6cbe9d
Eugene Kim <eugene0127@gmail.com>
2019-06-07 09:52:58 -0700

    removed 'CreateNewClass' and made changes accordingly to MyWindow.java

--- src/MyWindow.java
+++ src/MyWindow.java
@@ -2,6 +2,7 @@ import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.HashMap;
+
 import javax.swing.*;
 import java.util.*;
 
@@ -11,23 +12,53 @@ public class MyWindow extends JFrame{
 		JMenuItem newClass = new JMenuItem("New Class");
 		HashMap<String, EClass> hMap = new HashMap<>();
 		
+		
 	public MyWindow(String title) {
 		super(title);
 		setSize(800, 600);
 		setVisible(true);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
 		newClass.addActionListener(new ActionListener() {
-			
 			public void actionPerformed(ActionEvent e) {
+				JPanel prompt = new JPanel();
 				
+				JTextField nameField = new JTextField(7);
+				JTextField parentClassField = new JTextField(7);
+				//ArrayList of Children classes
+				//ArrayList of Methods
+				//ArrayList of Fields
+				
+				ArrayList<String> classNames = new ArrayList<String>();
+				Set<String> keys = hMap.keySet();
+				for (String key: keys) {
+					classNames.add(key);
+				}
+				
+				prompt.add(new JLabel("Class Name:"));
+				prompt.add(nameField);
+				prompt.add(new JLabel("Parent Class: "));
+				prompt.add(parentClassField);
+				
+				if (e.getSource() == newClass) {
+					JOptionPane.showConfirmDialog(null, prompt, "New Class", JOptionPane.OK_CANCEL_OPTION);
+				}
+				boolean noDups = true;
+				for (int i = 0; i < classNames.size(); i++) {
+					if (classNames.get(i).equals(parentClassField.getText())) {
+						JOptionPane.showMessageDialog(null, "A class with the same name already exist.", "Error", JOptionPane.QUESTION_MESSAGE);
+					}
+				}
+				
+				if (noDups) {
+					//make class and add to hashMap
+					//EClass newEClass = new EClass(nameField.getText(), );
+				}
 			}
 		});
+		System.out.println("checkout3");
 		option.add(newClass);
 		mBar.add(option);
 		setJMenuBar(mBar);
-		
-		setVisible(true);
 	}
 }
